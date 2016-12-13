fe91e6ff82df393e3eaead4c388337e85540eb5b
Nolan McShea <mcshean@bxscience.edu>
2016-12-13 13:18:48 -0500

    Chat Log
    Small chat log between the contributors

---
+++ minelog.java
@@ -0,0 +1,18 @@
+/* 
+ * Are we going to make booleans like
+ * boolean bomb
+ * boolean safe
+ * we only need one boolean smh
+ * 
+ * lol im so dumb i cant even
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
