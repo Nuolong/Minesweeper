66872a722f74ddea13c2249c165a1fc81930c7ab
stephaniechow <stephaniechow36@gmail.com>
2016-12-27 21:44:17 -0500

    Update MinesweeperTest.java

--- MinesweeperTest.java
+++ MinesweeperTest.java
@@ -5,8 +5,8 @@ public class MinesweeperTest{
     Scanner kb = new Scanner(System.in);
     System.out.println("How many rows would you like in your game? ");
     int r = kb.nextInt();
-    System.out.println("How many columns would you like? ");
-    int c = kb.nextint();
+    System.out.println("How many mines would you like? ");
+    int m = kb.nextint();
     Minesweeper arr = new Minesweeper(r,c);
     System.out.println("Pick a position: ");
   }
