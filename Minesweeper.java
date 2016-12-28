0a3e74d41a322d61e063eeb4580b47fe58ed785f
stephaniechow <stephaniechow36@gmail.com>
2016-12-27 21:28:43 -0500

    Add files via upload

---
+++ Minesweeper.java
@@ -0,0 +1,27 @@
+public class Minesweeper{
+  private int[][] arrans; //array with answers
+  private int[][] arrresult //array with input
+  
+  public Minsweeper(int s; int m){
+    arrans=new int[s][s];
+    for(int i = 0; i<m; i++){ //for loop for while the number of total mines is less than the number of mines needed
+      for(int r=0; r<arr.length; r++){
+        for(int c= 0; c< arr.length[r]; c++){
+          if((int)Math.random()+1 == 1 ){
+            arr[r][c] = 1;  //x is a mine
+          }
+        }
+      }
+    }
+  }
+  public boolean isMine(int q, int w){
+    if (arrans[q][w]==1){
+      arrresult[q][w]==1;
+      return true;
+    }
+    return false;
+  }
+  
+  public String toString(){}
+  
+}
---
+++ MinesweeperTest.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+
+public class MinesweeperTest{
+  public static void main(String[] args){
+  }
+}
