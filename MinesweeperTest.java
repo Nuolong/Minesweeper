baf00d0fa2cbdc8886f00f9d307fee9e49938320
Nolan McShea <mcshean@bxscience.edu>
2016-12-28 22:29:41 -0500

    Some updates
    I added to the selection of the spot for the user, and made a little bit for the case if there was a mine where the player selected. If so, the game is lost.
    
    Also I added a conditional so that an error doesn't occur in gameplay, that if there are an impossible number of mines (greater than the spots of field or equal to it), the game stops and gives a message

--- MinesweeperTest.java
+++ MinesweeperTest.java
@@ -6,8 +6,25 @@ public class MinesweeperTest{
     System.out.println("How many rows would you like in your game? ");
     int r = kb.nextInt();
     System.out.println("How many mines would you like? ");
-    int m = kb.nextint();
+    int m = kb.nextInt();
+    
+    if((r * r) <= m){
+      System.out.println("You specified too many mines! You can't win!");
+      //return SOMETHING I DON'T KNOW WHAT TO PUT HERE TO STOP EVERYTHING, I FORGOT. IS IT BREAK?
+    }
+    
     Minesweeper arr = new Minesweeper(r,m);
-    System.out.println("Pick a position: ");
+    System.out.println("Enter the row coordinate of the spot you would like to choose:");
+    int q = kb.nextInt();
+    System.out.println("Enter the column coordinate of the spot you would like to choose:");
+    int w = kb.nextInt();
+    
+    if(arr.isMine(q,w) == true){
+      System.out.println("Game over! You have selected a mine! ;w;");
+      //return SOMETHING I DON'T KNOW WHAT TO PUT HERE TO STOP EVERYTHING, I FORGOT. IS IT BREAK?
+    }
+    else{
+    }
+    
   }
 }
