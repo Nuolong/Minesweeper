dbca35b56e64c3836f038ae676782071ea92dcd5
Nolan McShea <mcshean@bxscience.edu>
2016-12-28 22:27:15 -0500

    Some changes in new branch
    I just put some spaces in between the operators so it is easier to read
    And I changed the first for loop for the mine generation into a while loop so it makes more sense if you think about it in English.
    
    Also I am not sure what the if statements are for, are they for checking the # of mines surrounding the selection? If so, should that be in another method?

--- Minesweeper.java
+++ Minesweeper.java
@@ -3,12 +3,14 @@ public class Minesweeper{
   private int[][] arrresult //array with input
   
   public Minsweeper(int s; int m){
-    arrans=new int[s][s];
-    for(int i = 0; i<m; i++){ //for loop for while the number of total mines is less than the number of mines needed
-      for(int r=0; r<arrans.length; r++){
-        for(int c= 0; c< arr.length[r]; c++){
-          if((int)Math.random()+1 == 1 ){
+    arrans = new int[s][s];
+    int a = 0;
+    while(a < m){ //WHILE LOOP FOR MINES ADDED TO FIELD
+      for(int r = 0; r < arrans.length; r++){
+        for(int c = 0; c < arr.length[r]; c++){
+          if((int)Math.random() + 1 == 1 ){
             arrans[r][c] = -1;  //x is a mine
+            a++; //INCREMENT TO END THE WHILE LOOP WHEN MINE QUANTITY IS FULFILLED
           }
           else{
             arrans[r][c] = 0; // means there is no mine
@@ -16,43 +18,43 @@ public class Minesweeper{
         }
       }
     }
-    for(int k=0; k<arrans.length;k++){
-      for(int j=0; j<arrans.length;j++){
-        if (arrans[k][j]==-1){
-          if(arrans[k][j+1]==0){
-            arrans[k][j+1]+=1;
+    for(int k = 0; k < arrans.length; k++){
+      for(int j = 0; j < arrans.length; j++){
+        if (arrans[k][j] == -1){
+          if(arrans[k][j + 1] == 0){
+            arrans[k][j + 1] += 1;
           }
-          if(arrans[k][j-1]==0){
-            arrans[k][j-1]+=1;
+          if(arrans[k][j- 1] == 0){
+            arrans[k][j - 1] += 1;
           }
-          if(arrans[k-1][j]==0){
-            arrans[k-1][j]+=1;
+          if(arrans[k - 1][j] == 0){
+            arrans[k - 1][j] += 1;
           }
-          if(arrans[k+1][j]==0){
-            arrans[k+1][j]+=1;
+          if(arrans[k + 1][j] == 0){
+            arrans[k + 1][j] += 1;
           }
-          if(arrans[k+1][j+1]==0){
-            arrans[k+1][j+1]+=1;
+          if(arrans[k + 1][j + 1] == 0){
+            arrans[k + 1][j + 1] += 1;
           }
-          if(arrans[k-1][j+1]==0){
-            arrans[k-1][j+1]+=1;
+          if(arrans[k - 1][ j+1 ] == 0){
+            arrans[k - 1][ j+1 ] += 1;
           }
-          if(arrans[k-1][j-1]==0){
-            arrans[k-1][j-1]+=1;
+          if(arrans[k - 1][j - 1] == 0){
+            arrans[k - 1][j - 1] += 1;
           }
-          if(arrans[k+1][j-1]==0){
-            arrans[k+1][j-1]+=1;
+          if(arrans[k + 1][j - 1] == 0){
+            arrans[k + 1][j - 1] += 1;
           }
         }
       }
     }
   }
   public boolean isMine(int q, int w){
-    if (arrans[q][w]==1){
-      arrresult[q][w]==1;
+    if (arrans[q][w] == 1){
+      arrresult[q][w] == 1;
       return true;
     }
-    arrresult=arrans;
+    arrresult = arrans;
     return false;
   }
   
