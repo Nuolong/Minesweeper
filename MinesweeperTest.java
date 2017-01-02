498cde7df150f5982eabca3e0244b9e0d79c5426
stephaniechow <stephaniechow36@gmail.com>
2017-01-02 00:58:21 -0500

    Update MinesweeperTest.java
    implements setflag and isfinish

--- MinesweeperTest.java
+++ MinesweeperTest.java
@@ -12,27 +12,31 @@ public class MinesweeperTest{
       System.out.println("You specified too many mines! You can't win!");
       return;
     }
-    
     Minesweeper arr = new Minesweeper(r,m);
-    System.out.println("Enter the row coordinate of the spot you would like to choose:");
-    int q = kb.nextInt();
-    System.out.println("Enter the column coordinate of the spot you would like to choose:");
-    int w = kb.nextInt();
-    System.out.println("What would you like to do?\n1 - Select\n2 - Flag\n3 - Unsure");
-    int s = kb.nextInt();
+    while(true){
+      System.out.println(arr);
+      System.out.println("Enter the row coordinate of the spot you would like to choose:");
+      int q = kb.nextInt();
+      System.out.println("Enter the column coordinate of the spot you would like to choose:");
+      int w = kb.nextInt();
+      System.out.println("What would you like to do?\n1 - Select\n2 - Flag");
+      int s = kb.nextInt();
     
-    if(s == 1){
-    if(arr.isMine(q,w) == true){
-      System.out.println("Game over! You have selected a mine! ;w;");
-      return;
+      if(s == 1){
+        if(arr.isMine(q,w) == true){
+          System.out.println(arr);
+          System.out.println("Game over! You have selected a mine! ;w;");
+          break;
+        } 
       }
-    if(){}  
-    }
-    else if(s == 2){
-    //*******//
-    }
-    else if(s == 3){
-    //*******//
+      else if(s == 2){
+        arr.setFlag(q,w);
+      }
+      if(arr.isFinish()== true){
+        System.out.println(arr);
+        System.out.println("Yay, you finished the game!!");
+          break;
+      }
     }
   }
 }
