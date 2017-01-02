a166dc321600c595ace84f6b67bce64fd5cc95bd
Nolan McShea <mcshean@bxscience.edu>
2017-01-01 19:57:22 -0500

    Giant wall of code
    Added 8 methods for each position surrounding a bomb
    Deleted the old if statements and mushed them into the new methods^
    Created a bunch of if statements using the methods for the conditions of the field to count the mine numbers in each unit not containing a mine:
    ~ Top left corner
    ~ Top edge excluding corners
    ~ Top right corner
    ~ Left edge excluding corners
    ~ Bottom left corner
    ~ Right edge excluding corners
    ~ Bottom right corner
    ~ Bottom edge excluding corners

--- Minesweeper.java
+++ Minesweeper.java
@@ -1,7 +1,8 @@
 public class Minesweeper{
+//-------------------------------------------------------//
   private int[][] arrans; //array with answers
   private int[][] arrresult //array with input
-  
+//-------------------------------------------------------//  
   public Minsweeper(int s; int m){
     arrans = new int[s][s];
     int a = 0;
@@ -9,7 +10,7 @@ public class Minesweeper{
       for(int r = 0; r < arrans.length; r++){
         for(int c = 0; c < arr.length[r]; c++){
           if((int)Math.random() + 1 == 1 ){
-            arrans[r][c] = -1;  //x is a mine
+            arrans[r][c] = -1;  //-1 is a mine
             a++; //INCREMENT TO END THE WHILE LOOP WHEN MINE QUANTITY IS FULFILLED
           }
           else{
@@ -18,46 +19,131 @@ public class Minesweeper{
         }
       }
     }
+//-------------------------------------------------------//    
     for(int k = 0; k < arrans.length; k++){
       for(int j = 0; j < arrans.length; j++){
-        if (arrans[k][j] == -1){
-          if(arrans[k][j + 1] == 0){
-            arrans[k][j + 1] += 1;
+        if(arrans[k][j] == -1){
+          if(k == 0){
+            if(j == 0){ //top left corner of the field
+              bottomMiddle(k,j);
+              bottomRight(k,j);
+              middleRight(k,j);
+            }
+            else if(j != 0 && j != arrans.length){ //the top edge of the field excluding the corners
+              middleLeft(k,j);
+              middleRight(k,j);
+              bottomLeft(k,j);
+              bottomMiddle(k,j);
+              bottomRight(k,j);
+            }
+            else if(j == arrans.length){ //top right corner of the field
+              middleLeft(k,j);
+              bottomLeft(k,j);
+              bottomMiddle(k,j);
+            }
           }
-          if(arrans[k][j- 1] == 0){
-            arrans[k][j - 1] += 1;
+          else if(j == 0){ 
+            if(k == arrans.length){ //bottom left corner of the field
+              topMiddle(k,j);
+              topRight(k,j);
+              middleRight(k,j);
+            }
+            if(k != 0 && k != arrans.length){ //the left edge of the field excluding the corners
+              topMiddle(k,j);
+              topRight(k,j);
+              middleRight(k,j);
+              bottomMiddle(k,j);
+              bottomRight(k,j);
+            }
           }
-          if(arrans[k - 1][j] == 0){
-            arrans[k - 1][j] += 1;
+          else if(j == arrans.length){
+            if(k != 0 && k != arrans.length){ //the right edge of the field excluding the corners
+              topMiddle(k,j);
+              topLeft(k,j);
+              middleLeft(k,j);
+              bottomLeft(k,j);
+              bottomMiddle(k,j);
+            }
+            else if(k == arrans.length){ //the bottom right corner of the field
+              topMiddle(k,j);
+              topLeft(k,j);
+              middleLeft(k,j);
+            }
           }
-          if(arrans[k + 1][j] == 0){
-            arrans[k + 1][j] += 1;
-          }
-          if(arrans[k + 1][j + 1] == 0){
-            arrans[k + 1][j + 1] += 1;
-          }
-          if(arrans[k - 1][ j+1 ] == 0){
-            arrans[k - 1][ j+1 ] += 1;
-          }
-          if(arrans[k - 1][j - 1] == 0){
-            arrans[k - 1][j - 1] += 1;
-          }
-          if(arrans[k + 1][j - 1] == 0){
-            arrans[k + 1][j - 1] += 1;
+          else if(k == arrans.length){
+            if(j != 0 && j != arrans.length){ //the bottom edge of the field exclusing the corner
+              middleLeft(k,j);
+              middleRight(k,j);
+              topLeft(k,j);
+              topMiddle(k,j);
+              topRight(k,j);
+            }
           }
         }
       }
-    }
-  }
+    }     
+//-------------------------------------------------------//        
   public boolean isMine(int q, int w){
-    if (arrans[q][w] == 1){
-      arrresult[q][w] == 1;
+    if (arrans[q][w] == -1){
+      arrresult= arrans;
       return true;
     }
-    arrresult = arrans;
+    arrresult[q][w] = arrans[q][w];
     return false;
   }
+//-------------------------------------------------------//
+  public void topLeft(int q, int w){
+    if(arrans[q - 1][w - 1] != -1){
+      arrans[q - 1][w - 1] += 1;
+    }
+  }
+//-------------------------------------------------------//       
+  public void topMiddle(int q, int w){
+    if(arrans[q - 1][w] != -1){
+      arrans[q - 1][w] += 1;
+    }
+  }
+//-------------------------------------------------------//
+  public void topRight(int q, int w){
+  if(arrans[q - 1][w + 1] != -1){
+      arrans[q - 1][w + 1] += 1;
+    }
+  }
+//-------------------------------------------------------//
+  public void middleLeft(int q, int w){
+  if(arrans[q][w - 1] != -1){
+      arrans[q][w - 1] += 1;
+    }
+  }
+//-------------------------------------------------------//
+  public void middleRight(int q, int w){
+  if(arrans[q][w + 1] != -1){
+      arrans[q][w + 1] += 1;
+    }
+  }
+//-------------------------------------------------------//
+  public void bottomLeft(int q, int w){
+  if(arrans[q + 1][w - 1] != -1){
+      arrans[q + 1][w - 1] += 1;
+    }
+  }
+//-------------------------------------------------------//
+  public void bottomMiddle(int q, int w){
+  if(arrans[q + 1][w] != -1){
+      arrans[q + 1][w] += 1;
+    }
+  }
+//-------------------------------------------------------//
+  public void bottomRight(int q, int w){
+  if(arrans[q + 1][w + 1] != -1){
+      arrans[q + 1][w + 1] += 1;
+    }
+  }
+//-------------------------------------------------------//
+
   
-  public String toString(){}
+  public String toString(){
+    
+  }
   
 }
