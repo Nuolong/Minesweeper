beada8cd7e8a293d59b01782cc61db13663335e7
stephaniechow <stephaniechow36@gmail.com>
2017-01-02 01:05:36 -0500

    Update MinesweeperTest.java
    added comments and commetary

--- MinesweeperTest.java
+++ MinesweeperTest.java
@@ -3,17 +3,18 @@ import java.util.Scanner;
 public class MinesweeperTest{
   public static void main(String[] args){
     Scanner kb = new Scanner(System.in);
+    System.out.println("Welcome to Minesweeper!\n In this game, you must select all spaces without a mine to win\nGood Luck!");
     System.out.println("How many rows would you like in your game? ");
     int r = kb.nextInt();
     System.out.println("How many mines would you like? ");
     int m = kb.nextInt();
     
-    if((r * r) <= m){
+    if((r * r) <= m){   //If there are more mines than the amount of spaces allowed, then the game stops
       System.out.println("You specified too many mines! You can't win!");
-      return;
+      break;
     }
     Minesweeper arr = new Minesweeper(r,m);
-    while(true){
+    while(true){  
       System.out.println(arr);
       System.out.println("Enter the row coordinate of the spot you would like to choose:");
       int q = kb.nextInt();
@@ -23,16 +24,16 @@ public class MinesweeperTest{
       int s = kb.nextInt();
     
       if(s == 1){
-        if(arr.isMine(q,w) == true){
+        if(arr.isMine(q,w) == true){     //If the player selects a mine, then the game ends.
           System.out.println(arr);
           System.out.println("Game over! You have selected a mine! ;w;");
           break;
         } 
       }
-      else if(s == 2){
+      else if(s == 2){       //sets a flag
         arr.setFlag(q,w);
       }
-      if(arr.isFinish()== true){
+      if(arr.isFinished()== true){    //if the player manages to find all the white spaces(spaces with no mines) the the game ends.
         System.out.println(arr);
         System.out.println("Yay, you finished the game!!");
           break;
