c910dd9740137a26ebaf52f721b816a4b4ff7183
Nolan McShea <mcshean@bxscience.edu>
2017-01-06 21:57:06 -0500

    Update Minesweeper.java

--- Minesweeper.java
+++ Minesweeper.java
@@ -12,17 +12,18 @@ public class Minesweeper{
     while(a < m){ //WHILE LOOP FOR MINES ADDED TO FIELD
       int x = (int) (Math.random() * s);
       int y = (int) (Math.random() * s);
-          if (arrans[x][y] != -1) 
-            {arrans[x][y] = -1;}
-          if (x > 0 && y > 0){arrans[x-1][y-1] += 1;}
-          if (x > 0){arrans[x-1][y] += 1;}
-          if (x > 0 && y < arrans[0].length - 1){arrans[x-1][y+1] += 1;}
-          if (y > 0){arrans[x][y-1] += 1;}
-          if (y < arrans[0].length - 1){arrans[x][y+1] += 1;}
-          if (x < arrans.length - 1 && y > 0){arrans[x+1][y-1] += 1;}
-          if (x < arrans.length - 1){arrans[x+1][y] += 1;}
-          if (x < arrans.length - 1 && y < arrans.length - 1){arrans[x+1][y+1] += 1;}
-          a++;
+      if (arrans[x][y] != -1) {
+        arrans[x][y] = -1;
+        
+        if (x > 0 && y > 0 && arrans[x-1][y-1] != -1) arrans[x-1][y-1] += 1;
+        if (x > 0 && arrans[x-1][y] != -1) arrans[x-1][y] += 1;
+        if (x > 0 && y < arrans[0].length - 1 && arrans[x-1][y+1] != -1) arrans[x-1][y+1] += 1;
+        if (y > 0 && arrans[x][y-1] != -1) arrans[x][y-1] += 1;
+        if (y < arrans[0].length - 1 && arrans[x][y+1] != -1) arrans[x][y+1] += 1;
+        if (x < arrans.length - 1 && y > 0 && arrans[x+1][y-1] != -1) arrans[x+1][y-1] += 1;
+        if (x < arrans.length - 1 && arrans[x+1][y] != -1) arrans[x+1][y] += 1;
+        if (x < arrans.length - 1 && y < arrans.length - 1 && arrans[x+1][y+1] != -1) arrans[x+1][y+1] += 1;
+        ++a;
       }
     }
     for(int z=0; z<arrresult.length; z++){
@@ -41,28 +42,28 @@ public class Minesweeper{
       return false;
     }
   }
-public void clearSpot(int x, int y) {
-  if (arrans[x][y] != 0) {
-    arrresult[x][y] = Integer.toString(arrans[x][y]);
-  } 
-  else if (arrresult[x][y].equals("-")) {
-     arrresult[x][y] = Integer.toString(arrans[x][y]);
-     if (x > 0 && y > 0){clearSpot(x-1, y-1);}
-     if (x > 0){clearSpot(x-1, y);}
-     if (x > 0 && y < arrans[0].length - 1){clearSpot(x-1, y+1);}
-     if (y > 0){clearSpot(x, y-1);}
-     if (y < arrans[0].length - 1){clearSpot(x, y+1);}
-     if (x < arrans.length - 1 && y > 0){clearSpot(x+1, y-1);}
-     if (x < arrans.length - 1){clearSpot(x+1, y);}
-     if (x < arrans.length - 1 && y < arrans.length - 1){clearSpot(x+1, y+1);}
-     }
-   }  
+  public void clearSpot(int x, int y) {
+    if (arrans[x][y] != 0) {
+      arrresult[x][y] = Integer.toString(arrans[x][y]);
+    } 
+    else if (arrresult[x][y].equals("-")) {
+      arrresult[x][y] = Integer.toString(arrans[x][y]);
+      if (x > 0 && y > 0){clearSpot(x-1, y-1);}
+      if (x > 0){clearSpot(x-1, y);}
+      if (x > 0 && y < arrans[0].length - 1){clearSpot(x-1, y+1);}
+      if (y > 0){clearSpot(x, y-1);}
+      if (y < arrans[0].length - 1){clearSpot(x, y+1);}
+      if (x < arrans.length - 1 && y > 0){clearSpot(x+1, y-1);}
+      if (x < arrans.length - 1){clearSpot(x+1, y);}
+      if (x < arrans.length - 1 && y < arrans.length - 1){clearSpot(x+1, y+1);}
+    }
+  }  
   
   public void printArray(){         
     for(int k=0; k< arrresult.length; k++){
       System.out.println("");
       for(int j=0; j< arrresult.length; j++){
-          System.out.print(" " + arrresult[k][j]+ " ");
+        System.out.print(" " + arrresult[k][j]+ " ");
       }
     }
   }
@@ -76,12 +77,10 @@ public void clearSpot(int x, int y) {
   }
   
   public boolean isFinished(){
-    int count=0;
+    int count = 0;
     for(int k = 0; k < arrresult.length; k++){
       for(int j = 0; j < arrresult.length; j++){
-        if(arrresult[k][j].equals("-") || arrresult[k][j].equals("!")){
-          ++count;
-        }
+        if(arrresult[k][j] == "-" || arrresult[k][j].equals("!")){++count;}
       }
     }
     return count==mines;
